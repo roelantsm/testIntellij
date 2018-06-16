56042e6b761e237227a29e499d50270b64084891
michiel roelants <michiel.1997@hotmail.com>
2018-06-16 15:28:16 +0200

    initial commit

---
+++ src/Logic/Main.java
@@ -0,0 +1,21 @@
+package Logic;
+
+
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        Date today2 = new Date();
+        String message = "dit is een message";
+
+        System.out.println(today2);
+        System.out.println(message);
+
+
+
+        String [] namen  = { "Michiel", "aa"};
+
+    }
+}
